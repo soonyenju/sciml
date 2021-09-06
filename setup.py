6cce266b170040536a3d8cac20141679eb456aa0
Zhu <soonyenju@foxmail.com>
2021-09-06 20:54:45 +0100

    initialize

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Songyan Zhu
+# Mail: zhusy93@gmail.com
+# Created Time:  2021-06-09 08:22
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "sciml",
+	version = "0.0.1",
+	keywords = ("Scientific machine learning wrappers"),
+	description = "draw and basic calculations/conversions",
+	long_description = "coming soon",
+	license = "MIT Licence",
+
+	url="https://github.com/soonyenju/sciml",
+	author = "Songyan Zhu",
+	author_email = "zhusy93@gmail.com",
+
+	packages = find_packages(),
+	include_package_data = True,
+	platforms = "any",
+	install_requires=[
+
+	]
+)
