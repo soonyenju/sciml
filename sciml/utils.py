1c517f387198ab8356f83aa33933299d02fb7d9c
Zhu <soonyenju@foxmail.com>
2021-09-06 21:19:07 +0100

    utils init

---
+++ sciml/utils.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import train_test_split
+
+
+def split(Xs, ys):
+    sss = ShuffleSplit(n_splits=1, test_size=0.33, random_state=42)
+    sss.get_n_splits(Xs, ys)
+    train_index, test_index = next(sss.split(Xs, ys)) 
+
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        Xs, ys, 
+        test_size = 0.33, 
+        random_state = 42
+    )
