1bbafe17ef702c03c4dc39706d7ee7cc98bd421e
Songyan Zhu <zhusy93@gmail.com>
2022-09-07 06:00:11 +0100

    Update utils.py

--- sciml/utils.py
+++ sciml/utils.py
@@ -2,16 +2,16 @@ from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import train_test_split
 
 
-def split(Xs, ys, return_index = False):
+def split(Xs, ys, return_index = False, test_size = 0.33, random_state = 42):
     if return_index:
-        sss = ShuffleSplit(n_splits=1, test_size=0.33, random_state=42)
+        sss = ShuffleSplit(n_splits=1, test_size = test_size, random_state = random_state)
         sss.get_n_splits(Xs, ys)
         train_index, test_index = next(sss.split(Xs, ys)) 
         return (train_index, test_index)
     else:
         X_train, X_test, y_train, y_test = train_test_split(
             Xs, ys, 
-            test_size = 0.33, 
-            random_state = 42
+            test_size = test_size, 
+            random_state = random_state
         )
         return (X_train, X_test, y_train, y_test)
